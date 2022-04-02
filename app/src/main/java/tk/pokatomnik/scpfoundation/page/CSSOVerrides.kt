1ca4d7b2713d4a5b98a3158bb71b94df07b79f88
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2022-04-03 01:51:06 +0300

    fix page size

---
+++ app/src/main/java/tk/pokatomnik/scpfoundation/page/CSSOVerrides.kt
@@ -0,0 +1,19 @@
+package tk.pokatomnik.scpfoundation.page
+
+val css = """
+    /* Hide those elements */
+    #header { display: none !important; }
+    #container-wrap { background: transparent !important; }
+    .page-tags { display: none !important; }
+    #page-info-break { display: none !important; }
+    #page-options-container { display: none !important; }
+    #action-area { display: none !important; }
+    #footer { display: none !important; }
+    #license-area { display: none !important; }
+    #side-bar { display: none !important; }
+    .page-rate-widget-box { display: none !important; }
+    
+    /* Style overrides */
+    #page-content { min-height: 0 !important; }
+    #main-content { margin: 0 20px !important; }
+""".trimIndent()
--- app/src/main/java/tk/pokatomnik/scpfoundation/page/Page.kt
+++ app/src/main/java/tk/pokatomnik/scpfoundation/page/Page.kt
@@ -16,23 +16,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-val css = """
-    /* Hide those elements */
-    #header { display: none !important; }
-    #container-wrap { background: transparent !important; }
-    .page-tags { display: none !important; }
-    #page-info-break { display: none !important; }
-    #page-options-container { display: none !important; }
-    #action-area { display: none !important; }
-    #footer { display: none !important; }
-    #license-area { display: none !important; }
-    #side-bar { display: none !important; }
-    .page-rate-widget-box { display: none !important; }
-    
-    /* Style overrides */
-    #page-content { min-height: 0 !important; }
-""".trimIndent()
-
 enum class PageState {
     INITIAL,
     LOADING,
