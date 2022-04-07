f0992c905e46feb0e9d8ce1f9f4df9ca4c8ef24c
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2022-04-07 10:40:02 +0300

    remember current page using shared preferences

--- app/src/main/java/tk/pokatomnik/scpfoundation/pages/PagesViewModel.kt
+++ app/src/main/java/tk/pokatomnik/scpfoundation/pages/PagesViewModel.kt
@@ -3,9 +3,11 @@ package tk.pokatomnik.scpfoundation.pages
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import tk.pokatomnik.scpfoundation.di.http.HttpClient
+import tk.pokatomnik.scpfoundation.di.preferences.PreferencesContainer
 import javax.inject.Inject
 
 @HiltViewModel
 class PagesViewModel @Inject constructor(
-    val httpClient: HttpClient
+    val httpClient: HttpClient,
+    val preferencesContainer: PreferencesContainer
 ) : ViewModel()
