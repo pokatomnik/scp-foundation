71529aee214cfae378c064bb156232f686d72c15
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2022-04-09 00:23:19 +0300

    Fix pages list refresh when no items are available

--- app/src/main/java/tk/pokatomnik/scpfoundation/pages/LazyPagesList.kt
+++ app/src/main/java/tk/pokatomnik/scpfoundation/pages/LazyPagesList.kt
@@ -12,8 +12,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.swiperefresh.SwipeRefresh
-import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import kotlinx.coroutines.launch
 import tk.pokatomnik.scpfoundation.components.LazyList
 import tk.pokatomnik.scpfoundation.di.db.dao.favorites.Favorite
@@ -24,13 +22,10 @@ import tk.pokatomnik.scpfoundation.domain.PagedResponse
 @Composable
 fun LazyPagesList(
     loading: Boolean = false,
-    forceRefresh: () -> Unit = {},
     pagedResponse: PagedResponse,
     onSelectURL: (url: String) -> Unit = {}
 ) {
     val scope = rememberCoroutineScope()
-
-    val scrollRefreshState = rememberSwipeRefreshState(loading)
     val favoritesState = remember { mutableStateListOf<String>() }
     val database = rememberDatabase()
 
@@ -56,57 +51,53 @@ fun LazyPagesList(
         }
     }
 
-    SwipeRefresh(
-        state = scrollRefreshState,
-        onRefresh = forceRefresh,
-        modifier = Modifier.fillMaxSize(),
-        swipeEnabled = !loading
+
+    LazyList(
+        list = pagedResponse.pages,
+        onClick = { onSelectURL(it.url) },
+        disabled = loading,
     ) {
-        LazyList(
-            list = pagedResponse.pages,
-            onClick = { onSelectURL(it.url) },
-            disabled = loading,
-        ) {
-            Row {
-                Column(modifier = Modifier.weight(1f)) {
-                    Row { Text(it.name, maxLines = 1, overflow = TextOverflow.Ellipsis) }
-                    Row {
-                        Text(
-                            it.author ?: "(Автор неизвестен)",
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis
-                        )
-                    }
+        Row {
+            Column(modifier = Modifier.weight(1f)) {
+                Row { Text(it.name, maxLines = 1, overflow = TextOverflow.Ellipsis) }
+                Row {
+                    Text(
+                        it.author ?: "(Автор неизвестен)",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
                 }
-                Column(modifier = Modifier
+            }
+            Column(
+                modifier = Modifier
                     .requiredWidth(48.dp)
-                    .width(48.dp)) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center
-                    ) {
-                        IconButton(
-                            modifier = Modifier
-                                .requiredWidth(48.dp)
-                                .width(48.dp),
-                            onClick = {
-                                if (favoritesState.contains(it.url)) {
-                                    removeFavorite(it)
-                                } else {
-                                    addFavorite(it)
-                                }
-                            }
-                        ) {
-                            val icon = if (favoritesState.contains(it.url)) {
-                                Icons.Filled.Favorite
+                    .width(48.dp)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    IconButton(
+                        modifier = Modifier
+                            .requiredWidth(48.dp)
+                            .width(48.dp),
+                        onClick = {
+                            if (favoritesState.contains(it.url)) {
+                                removeFavorite(it)
                             } else {
-                                Icons.Filled.FavoriteBorder
+                                addFavorite(it)
                             }
-                            Icon(
-                                imageVector = icon,
-                                contentDescription = "В Избранное"
-                            )
                         }
+                    ) {
+                        val icon = if (favoritesState.contains(it.url)) {
+                            Icons.Filled.Favorite
+                        } else {
+                            Icons.Filled.FavoriteBorder
+                        }
+                        Icon(
+                            imageVector = icon,
+                            contentDescription = "В Избранное"
+                        )
                     }
                 }
             }
