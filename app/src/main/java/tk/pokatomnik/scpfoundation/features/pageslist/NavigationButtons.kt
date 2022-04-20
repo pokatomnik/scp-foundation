499b4cc2aa7f7d029682d9b218f2428fd5bda3dc
Danilian Akhmedzianov <dakhmedzyanov@samokat.ru>
2022-04-20 15:12:04 +0300

    fix dialog layout

--- app/src/main/java/tk/pokatomnik/scpfoundation/features/pageslist/NavigationButtons.kt
+++ app/src/main/java/tk/pokatomnik/scpfoundation/features/pageslist/NavigationButtons.kt
@@ -1,9 +1,12 @@
 package tk.pokatomnik.scpfoundation.features.pageslist
 
+import android.widget.Space
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -13,7 +16,10 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 
@@ -28,37 +34,51 @@ internal fun NavigationButtons(
     var dialogVisible by remember { mutableStateOf(false) }
     var directPageNumberInput by remember { mutableStateOf(currentPage.toString()) }
 
+    fun handleNavigationDone() = try {
+        onExplicitNavigate(directPageNumberInput.toInt())
+        dialogVisible = false
+        directPageNumberInput = currentPage.toString()
+    } catch (e: Exception) {
+        Toast.makeText(context, "Введите целое число", Toast.LENGTH_SHORT).show()
+    }
+
     if (dialogVisible) {
         AlertDialog(
             onDismissRequest = { dialogVisible = false },
-            title = { Text("Перейти к странице") },
             text = {
-                TextField(
-                    singleLine = true,
-                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-                    value = directPageNumberInput,
-                    onValueChange = { directPageNumberInput = it }
-                )
+                Column(modifier = Modifier.fillMaxWidth().width(IntrinsicSize.Min).padding(horizontal = 8.dp)) {
+                    Text(
+                        text = "Перейти к странице",
+                        fontWeight = FontWeight.Bold
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    OutlinedTextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        placeholder = { Text("Номер страницы") },
+                        singleLine = true,
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            keyboardType = KeyboardType.Number,
+                            imeAction = ImeAction.Done
+                        ),
+                        keyboardActions = KeyboardActions(
+                            onDone = { handleNavigationDone() }
+                        ),
+                        value = directPageNumberInput,
+                        onValueChange = { directPageNumberInput = it },
+                    )
+                }
             },
             buttons = {
                 Row(
-                    modifier = Modifier.padding(all = 8.dp),
+                    modifier = Modifier.fillMaxWidth().padding(horizontal = 32.dp),
                     horizontalArrangement = Arrangement.Center,
                 ) {
                     Button(
-                        modifier = Modifier.fillMaxWidth(),
-                        onClick = {
-                            try {
-                                onExplicitNavigate(directPageNumberInput.toInt())
-                                dialogVisible = false
-                                directPageNumberInput = currentPage.toString()
-                            } catch (e: Exception) {
-                                Toast.makeText(context, "Введите целое число", Toast.LENGTH_SHORT).show()
-                            }
-                        }
+                        onClick = { handleNavigationDone() },
+                        modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)
                     ) {
-                        Text("Перейти")
-                    }
+                            Text("Перейти")
+                        }
                 }
             }
         )
