0551e8757b071c164360ea6f2edc4e3a7e81b5c1
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2022-05-03 17:29:20 +0300

    fix dialog theme

--- app/src/main/java/tk/pokatomnik/scpfoundation/features/pageslist/NavigationButtons.kt
+++ app/src/main/java/tk/pokatomnik/scpfoundation/features/pageslist/NavigationButtons.kt
@@ -1,11 +1,8 @@
 package tk.pokatomnik.scpfoundation.features.pageslist
 
-import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.SkipNext
@@ -14,12 +11,10 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import tk.pokatomnik.scpfoundation.components.NumberPickerComponent
+import androidx.compose.ui.window.Dialog
+import com.chargemap.compose.numberpicker.NumberPicker
 
 @Composable
 internal fun NavigationButtons(
@@ -37,32 +32,19 @@ internal fun NavigationButtons(
     }
 
     if (dialogVisible) {
-        AlertDialog(
-            onDismissRequest = { dialogVisible = false },
-            title = {
+        Dialog(onDismissRequest = { dialogVisible = false }) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .width(IntrinsicSize.Min)
+                    .padding(horizontal = 8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
                 Text(
                     text = "Перейти к странице",
                     fontWeight = FontWeight.Bold
                 )
-            },
-            text = {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .width(IntrinsicSize.Min)
-                        .padding(horizontal = 8.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    NumberPickerComponent(
-                        max = maxPage,
-                        min = 1,
-                        onValueChange = { _, new ->
-                            directPageNumberInput = new
-                        }
-                    )
-                }
-            },
-            buttons = {
+                NumberPicker(value = directPageNumberInput, onValueChange = { directPageNumberInput = it }, range = 1..maxPage)
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -79,7 +61,7 @@ internal fun NavigationButtons(
                     }
                 }
             }
-        )
+        }
     }
 
     Divider(modifier = Modifier.fillMaxWidth())
